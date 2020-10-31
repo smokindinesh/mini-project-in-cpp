03fce266d96b7c79233278dc52c46e532d3fff1c
neerajdhakad <dhakadneeraj786@gmail.com>
2020-10-31 18:31:53 +0530

    main.cpp

--- busReservation/main.cpp
+++ busReservation/main.cpp
@@ -341,6 +341,10 @@ void a::avail()
 int main()
 
 {
+  ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+  //Above 3 lines will fast the Input and output processes
 
 system("cls");
 
